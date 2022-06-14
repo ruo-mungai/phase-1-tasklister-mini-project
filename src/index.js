f0d08311b0d08aa17f250100dc3869a441f26462
Patrick <ruomungai@gmail.com>
2022-06-14 16:31:39 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form=document.querySelector('form')
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+ handleToDo(e.target.description.value) // Changed new-task-description id to description on html.
+ form.reset()
+})
+
 });
+
+function handleToDo(todo){
+  let p=document.createElement('p')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent='x'
+  p.textContent=`${todo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
